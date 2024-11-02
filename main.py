6717c58e9a1124a1028965293181d9508afb8ba9
lucas-lacerda2001 <lucaslacerdads2001@gmail.com>
2024-11-02 10:41:36 -0300

    Área de mensagem adicionada informando os dados salvos; Ajustes na janela

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ from openpyxl import Workbook, load_workbook
 import os
 
 # - Funções
-
-def salvar_dados_excel(data_hora, temperatura, umidade):
+def salvar_dados_na_planilha(data_hora, temperatura, umidade):
   # Verifica se o arquivo já existe
   if os.path.exists("historico_temperatura.xlsx"):
     # Abre o arquivo existente
@@ -30,31 +29,30 @@ def salvar_dados_excel(data_hora, temperatura, umidade):
 
 def salvar_temperatura():
   # Inicialização do WebDriver
-  driver_path = "./static/web_drivers/msedgedriver"
-  driver = webdriver.Edge()
+  driver = webdriver.Edge() # Nesse caso, o caminho do WebDriver está salvo como variável do sistema.
 
-  # Navegação até a página do tempo
-  driver.get("https://weather.com/pt-BR/clima/hoje/l/ebe93c0e09d0cfe19844d4281461901cd8f083c310e64255954758c8dcab784b")
+  # Navegação até a página de temperatura
+  driver.get("https://www.google.com.br/search?q=temperatura+s%C3%A3o+paulo")
 
   # Extração da temperatura e umidade
-  elemento_temperatura = driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/main/div[4]/section/div/div[1]/div[1]/span[2]/span').text
-  elemento_umidade = driver.find_element(By.XPATH, '/html/body/div[1]/main/div[2]/main/div[4]/section/div/div[2]/div[3]/div[2]/span')
+  elemento_temperatura = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[13]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[1]/div[1]/div/div[1]/span[1]')
+  elemento_umidade = driver.find_element(By.XPATH, '/html/body/div[3]/div/div[13]/div/div[2]/div[2]/div/div/div[1]/div/div/div/div/div[1]/div[2]/div[2]/span')
 
-  temperatura = elemento_temperatura if isinstance(elemento_temperatura, str) else elemento_temperatura.text
+  # Conversão de elemento HTML para texto (caso necessário)
+  temperatura = elemento_temperatura if isinstance(elemento_temperatura, str) else elemento_temperatura.text + '°C'
   umidade = elemento_umidade if isinstance(elemento_umidade, str) else elemento_umidade.text
 
-  print(temperatura)
-  print(umidade)
-  
   # Captura da data e hora atuais
   data_hora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 
+  # Salvar os dados na planilha
+  salvar_dados_na_planilha(data_hora, temperatura, umidade)
+
+  # Mostra os dados salvos na label de mensagens
+  label_mensagem.config(text=f"Dados salvos:\n{data_hora}\nTemperatura: {temperatura}\nUmidade: {umidade}")
+
   # Fechar o navegador
   driver.quit()
-
-  # Salvar os dados na planilha
-  salvar_dados_excel(data_hora, temperatura, umidade)
-
 
 # - Configurações da janela
 # Criação
@@ -64,7 +62,7 @@ janela = tk.Tk()
 janela.title("Weather Now")
 
 # Tamanho da janela
-janela.geometry("300x200")
+janela.geometry("300x260")
 
 # Impedir o redimensionamento
 janela.resizable(False, True)
@@ -82,11 +80,13 @@ label_imagem.pack()
 label = tk.Label(janela, text="Atualize a situação do clima em São Paulo!")
 label.pack() # Adicionar o widget à janela
 
-# - Botões
 # Botão Salvar Clima
 botao = tk.Button(janela, text="Buscar previsão", command=salvar_temperatura)
-botao.pack()
+botao.pack(pady="8px")
 
+# Mensagem
+label_mensagem = tk.Label(janela, text="")
+label_mensagem.pack(pady="5px")
 
 # - Loop da aplicação para exibir a janela
 janela.mainloop()
